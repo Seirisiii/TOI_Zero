2db552e97585e5815c24836b915113fc17805b59
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2025-04-10 19:48:17 +0700

    A1-004

---
+++ A1/cpp/004.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    if(a>=5 and b>=20 and c>=25)cout<<"pass";
+    else cout<<"fail";
+    return 0;
+}
