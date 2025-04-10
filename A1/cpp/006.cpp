8a695192231ec49d76473de02e24b511ade95b4b
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2025-04-10 20:02:53 +0700

    A1-006

---
+++ A1/cpp/006.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    if(a%b==0)cout<<"yes";
+    else cout<<"no";
+    return 0;
+}
